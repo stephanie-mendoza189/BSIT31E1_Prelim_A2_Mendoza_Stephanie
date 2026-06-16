925f9245ddd12d161deffc5fc04033282876cb13
stephanie-mendoza189 <tepteptep231@gmail.com>
2026-06-16 11:05:20 +0800

    enter
    enter number and operator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+while (true)
+{
+    Console.Write("Enter first number: ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+        break;
+
+    double num1 = Convert.ToDouble(input);
+
+    Console.Write("Enter second number: ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter operator: ");
+    string operatorr = Console.ReadLine();
+
+    double result = 0;
+
+    
+}
+
+
+    
